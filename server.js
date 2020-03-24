bb580d59be6449c0eab03f8903e6238d330e6ecf
C-Wally97 <connorwallace87@gmail.com>
2020-03-24 14:01:56 +0000

    added db + results.

--- server.js
+++ server.js
@@ -15,22 +15,5 @@ async function getReq(req, res) {
     res.json(response)
 }
 
-// async function editDesc(req, res) {
-//     req.body.id = req.params.id;
-//     req.body.closed = (req.body.closed == 'true')
-//     if (session.auth) {
-//         let response = await sqlDb.editDesc(req.body)
-//         if(response != null) {
-//             res.redirect('/descriptions')
-//         }     
-//         else {     
-//             res.render('descriptions', {data: {message: 'Adding failed'}})
-//         }
-        
-//     }
-//     else {
-//          res.redirect('/')
-//     }
-// }
 
 // echo "GET http://localhost:8080/get/10" | vegeta attack -rate=10/s -duration=10s | vegeta encode > results.json
